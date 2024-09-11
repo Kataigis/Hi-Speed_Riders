643a8e2c06ff376cd78a5eb3b5361a5330df8377
Kataigis <111102572+Kataigis@users.noreply.github.com>
2024-09-10 21:54:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,45 +1,103 @@
 $(document).ready(function () {
-    document.getElementById("homebtn").addEventListener('click', function () {
+    document.getElementById("btn_home").addEventListener('click', function () {
         pageTransition("home");
     });
 
-    document.getElementById("corebtn").addEventListener('click', function () {
+    document.getElementById("btn_core").addEventListener('click', function () {
         pageTransition("core");
     });
 
-    document.getElementById("enginebtn").addEventListener('click', function (e) {
-        pageTransition("engine");
+    document.getElementById("btn_generics").addEventListener('click', function (e) {
+        pageTransition("generics");
     });
 
-    document.getElementById("evolbtn").addEventListener('click', function (e) {
-        pageTransition("engine");
-        document.getElementById("evolcontent").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("btn_g_main").addEventListener('click', function (e) {
+        pageTransition("generics");
+        document.getElementById("content_g_main").scrollIntoView({ behavior: 'smooth' });
     });
 
-    document.getElementById("raikabtn").addEventListener('click', function (e) {
-        pageTransition("engine");
-        document.getElementById("raikacontent").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("btn_g_extra").addEventListener('click', function (e) {
+        pageTransition("generics");
+        document.getElementById("content_g_extra").scrollIntoView({ behavior: 'smooth' });
     });
 
-    document.getElementById("annebtn").addEventListener('click', function (e) {
-        pageTransition("engine");
-        document.getElementById("annecontent").scrollIntoView({ behavior: 'smooth' });
+    document.getElementById("btn_variants").addEventListener('click', function (e) {
+        pageTransition("variants");
     });
 
-    document.getElementById("techbtn").addEventListener('click', function () {
-        pageTransition("tech");
+    document.getElementById("btn_v_dragon").addEventListener('click', function (e) {
+        pageTransition("variants");
+        document.getElementById("content_v_dragon").scrollIntoView({ behavior: 'smooth' });
     });
 
-    document.getElementById("listbtn").addEventListener('click', function () {
-        pageTransition("list");
+    document.getElementById("btn_v_wasp").addEventListener('click', function (e) {
+        pageTransition("variants");
+        document.getElementById("content_v_wasp").scrollIntoView({ behavior: 'smooth' });
     });
 
-    document.getElementById("combobtn").addEventListener('click', function () {
-        pageTransition("combo");
+    document.getElementById("btn_v_magnet").addEventListener('click', function (e) {
+        pageTransition("variants");
+        document.getElementById("content_v_magnet").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_v_kash").addEventListener('click', function (e) {
+        pageTransition("variants");
+        document.getElementById("content_v_kash").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_v_symph").addEventListener('click', function (e) {
+        pageTransition("variants");
+        document.getElementById("content_v_symph").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_v_trap").addEventListener('click', function (e) {
+        pageTransition("variants");
+        document.getElementById("content_v_trap").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_engines").addEventListener('click', function (e) {
+        pageTransition("engines");
+    });
+
+    document.getElementById("btn_e_gunity").addEventListener('click', function (e) {
+        pageTransition("engines");
+        document.getElementById("content_e_gunity").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_e_kash").addEventListener('click', function (e) {
+        pageTransition("engines");
+        document.getElementById("content_e_kash").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_e_machina").addEventListener('click', function (e) {
+        pageTransition("engines");
+        document.getElementById("content_e_machina").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_e_symph").addEventListener('click', function (e) {
+        pageTransition("engines");
+        document.getElementById("content_e_symph").scrollIntoView({ behavior: 'smooth' });
+    });
+
+    document.getElementById("btn_lists").addEventListener('click', function () {
+        pageTransition("lists");
+    });
+
+    document.getElementById("btn_combos").addEventListener('click', function () {
+        pageTransition("combos");
+    });
+
+    document.getElementById("btn_md").addEventListener('click', function () {
+        pageTransition("md");
+    });
+
+    document.getElementById("btn_discussions").addEventListener('click', function () {
+        pageTransition("discussions");
     });
 });
 
 function pageTransition(id) {
+    window.scrollTo(0, 0);
     var btns = document.getElementsByClassName("nav-link");
     var contents = document.getElementsByClassName("tabContent");
     Array.from(btns).forEach((el) => {
@@ -49,8 +107,6 @@ function pageTransition(id) {
     Array.from(contents).forEach((el) => {
         el.style.display = "none";
     })
-    window.scrollTo(0, 0);
-    document.getElementById(`${id}btn`).classList.add("active");
-    $(`#${id}content`).fadeIn(600);
-    console.log(`${id}btn ${id}content`);
+    document.getElementById(`btn_${id}`).classList.add("active");
+    $(`#content_${id}`).fadeIn(600);
 }
